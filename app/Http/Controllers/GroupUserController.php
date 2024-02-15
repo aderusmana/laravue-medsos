e2c8a1418d1caa894a71ebbd84e1f66435e51507
aderusmana <aderusmana777@gmail.com>
2024-02-15 23:04:37 +0700

    setup laravel,docker and generate model, migration and controller

---
+++ app/Http/Controllers/GroupUserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GroupUser;
+use Illuminate\Http\Request;
+
+class GroupUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GroupUser $groupUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(GroupUser $groupUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, GroupUser $groupUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GroupUser $groupUser)
+    {
+        //
+    }
+}
