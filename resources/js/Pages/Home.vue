23b001507660b18947ae6befa45069c175d626aa
aderusmana <aderusmana777@gmail.com>
2024-02-19 16:08:41 +0700

    Clean unused route and view

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+  
+});
+</script>
+
+<template>
+    <Head title="BuchinBook" />
+
+
+</template>
+
+<style>
+
+</style>
